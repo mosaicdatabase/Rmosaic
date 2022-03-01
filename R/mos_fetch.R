c47cd15e504af9b64ad49e4301767cbf28ec5360
mosaic database <99194994+mosaicdatabase@users.noreply.github.com>
2022-03-01 14:20:21 +0000

    Update mos_fetch.R

--- R/mos_fetch.R
+++ R/mos_fetch.R
@@ -17,7 +17,7 @@ install.load.package <- function(x) { # Automate installs & load packages from C
 }
 
 package_vec <- c( # vector of package/library names - note: CRAN-dependent (no GitHub, local, &c.)
-  "Rcurl",
+  "RCurl",
   "Rcompadre"
 )
 
